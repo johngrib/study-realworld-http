4b681902057ad56d34c8b4fbebdec299cdbcaa9e
John Grib <johngrib82@gmail.com>
2019-06-16 18:51:41 +0900

    Example 3-4. Request GET with query

--- recipe/common.go
+++ recipe/common.go
@@ -4,14 +4,33 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 )
 
-func RequestGet(url string) {
-	resp, err := http.Get(url)
+func RequestGet(uriAddress string) {
+	resp, err := http.Get(uriAddress)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(body))
+}
+
+func RequestGetWithQuery(uriAddress string) {
+	values := url.Values{
+		"query": {"hello world"},
+	}
+
+	resp, err := http.Get(uriAddress + "?" + values.Encode())
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
