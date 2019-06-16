c5f4686bd99af3f59106ea5ef8b320e46ebfca82
John Grib <johngrib82@gmail.com>
2019-06-16 18:43:58 +0900

    Example 3-1. get Body by GET method

---
+++ recipe/common.go
@@ -0,0 +1,20 @@
+package recipe
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func RequestGet(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(body))
+}
